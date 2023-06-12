1a1afa904d7f1918fd470c2f8328eb1ea1ff6d6c
Martin Alvaro <martinalvaro3175@gmail.com>
2023-06-12 17:19:20 -0300

    segundo desafio

--- productManager.js
+++ productManager.js
@@ -1,53 +1,122 @@
+const fs = require('fs');
+
 class ProductManager {
-    constructor() {
+  constructor(path) {
+    this.path = path;
+    this.products = [];
+  }
+
+  addProduct(title, description, price, thumbnail, code, stock) {
+    if (!title || !description || !price || !thumbnail || !code || !stock) {
+      return 'Please complete all required fields!!';
+    }
+
+    try {
+      this.getProducts();
+
+      const productExist = this.products.find((product) => product.code === code);
+      if (productExist) {
+        return `The product code already exists: ${code}`;
+      }
+
+      const id = this.products.length + 1;
+
+      const newProduct = {
+        id,
+        title,
+        description,
+        price,
+        thumbnail,
+        code,
+        stock,
+      };
+
+      this.products.push(newProduct);
+
+      fs.writeFileSync(this.path, JSON.stringify(this.products));
+
+      console.log('Product added!!');
+      return newProduct;
+    } catch (error) {
+      console.error('Error adding product:', error);
+      return 'An error occurred while adding the product.';
+    }
+  }
+
+  getProducts() {
+    try {
+      if (fs.existsSync(this.path)) {
+        const data = fs.readFileSync(this.path, 'utf8');
+        this.products = JSON.parse(data);
+      } else {
         this.products = [];
+      }
+    } catch (error) {
+      console.error('Error reading products:', error);
+      this.products = [];
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            return 'Faltan completar campos!!'
-        }
-        const productExist = this.products.find((product) => product.code === code)
-        if (productExist) {
-            return `El codigo del producto ya existe: ${code}`
-        }
+    return this.products;
+  }
 
-        const id = this.products.length + 1
+  getProductById(id) {
+    try {
+      this.getProducts();
 
-        const newProduct = {
-            id,
-            title,
-            description,
-            price,
-            thumbnail,
-            code,
-            stock,
-        }
-
-        this.products.push(newProduct)
-
-        console.log("Producto agregado con exito!!")
-        return newProduct
-
+      const productId = this.products.find((product) => product.id === id);
+      if (productId) {
+        console.log(`Product found: ${id}`);
+        return productId;
+      } else {
+        return 'Not found';
+      }
+    } catch (error) {
+      console.error(error);
+      return 'Error getting product by ID:';
     }
+  }
+  updateProduct(id, data) {
+    try {
+      this.getProducts();
 
-    getProducts() {
-        return this.products;
+      const productIndex = this.products.findIndex((product) => product.id === id);
+      if (productIndex !== -1) {
+        this.products[productIndex] = { ...this.products[productIndex], ...data };
+        fs.writeFileSync(this.path, JSON.stringify(this.products));
+        console.log(`Product updated: ${id}`);
+        return this.products[productIndex];
+      } else {
+        return 'Product not found';
+      }
+    } catch (error) {
+      console.error(error);
+      return 'Error updating product:';
     }
+  }
 
-    getProductById(id){
-        const productId = this.products.find((product)=> product.id === id)
-        if(productId){
-            console.log(`Producto encontrado: ${id}`)
-            return productId
-        }else{
-            return "Not found"
-        }
+  deleteProduct(id) {
+    try {
+      this.getProducts();
+
+      const productIndex = this.products.findIndex((product) => product.id === id);
+      if (productIndex !== -1) {
+        const deletedProduct = this.products.splice(productIndex, 1);
+        fs.writeFileSync(this.path, JSON.stringify(this.products));
+        console.log(`Product deleted: ${id}`);
+        return deletedProduct;
+      } else {
+        return 'Product not found';
+      }
+    } catch (error) {
+      console.error('Error deleting product:', error);
+      return 'An error occurred while deleting the product.';
     }
-    
+  }
+  
 }
 
-const product = new ProductManager()
+
+const product = new ProductManager('./data/products.json')
 
 //agrego productos
 console.log(product.addProduct('Camiseta Brasil', 'Descripcion del producto 1', 15000, 'imagen1.jpg', 1, 32))
@@ -59,10 +128,50 @@ console.log(product.addProduct('Camiseta Francia', 'Descripcion del producto 6',
 
 
 //busco todos los productos
-console.log(product.getProducts())
+ console.log(product.getProducts())
 
 
 //busco producto por id
 console.log(product.getProductById(4))//existe
+
+
 //busco producto por id
 console.log(product.getProductById(5)) //no existe
+
+
+//actualizo producto (existe)
+console.log(product.updateProduct(2, {
+  title: 'Camiseta francia Actualizada',
+  description: 'Descripción actualizada',
+  price: 13999,
+  thumbnail: 'imagen2_actualizada.jpg',
+  code: 12,
+  stock: 10,
+}));
+
+
+//actualizo producto (no existe)
+console.log(product.updateProduct(22, {
+  title: 'Camiseta actualizada',
+  description: 'Descripción actualizada',
+  price: 13999,
+  thumbnail: 'img.jpg',
+  code: 12,
+  stock: 10,
+}));
+
+
+//busco todos los productos
+console.log(product.getProducts())
+
+
+//elimino producto (existe)
+console.log(product.deleteProduct(2))
+
+
+//elimino un producto (no existe)
+console.log(product.deleteProduct(6))
+
+
+//busco todos los elementos
+console.log(product.getProducts())
