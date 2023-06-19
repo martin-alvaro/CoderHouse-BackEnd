b73810dc73c4c04363abd7002fbea66e2d60f4d6
Martin Alvaro <martinalvaro3175@gmail.com>
2023-06-19 16:53:09 -0300

    tercer desafio

--- src/productManager.js
+++ src/productManager.js
@@ -1,5 +1,4 @@
-const fs = require('fs');
-
+import fs from 'fs'
 class ProductManager {
   constructor(path) {
     this.path = path;
@@ -108,70 +107,75 @@ class ProductManager {
         return 'Product not found';
       }
     } catch (error) {
-      console.error('Error deleting product:', error);
-      return 'An error occurred while deleting the product.';
+      console.error(error);
+      return 'Error deleting product:';
     }
   }
   
 }
 
 
-const product = new ProductManager('./data/products.json')
+// const product = new ProductManager('./data/products.json');
+
+
 
 //agrego productos
-console.log(product.addProduct('Camiseta Brasil', 'Descripcion del producto 1', 15000, 'imagen1.jpg', 1, 32))
-console.log(product.addProduct('Camiseta Alemania', 'Descripcion del producto 2', 12999, 'imagen2.jpg', 12, 17))
-console.log(product.addProduct('Camiseta Italia', 'Descripcion del producto 3', 12500, 'imagen3.jpg', 123, 14))
-console.log(product.addProduct('Camiseta Argentina', 'Descripcion del producto 4', 13000 , 'imagen1.jpg', 1234, 25))
-console.log(product.addProduct('Camiseta Uruguay', 'Descripcion del producto 5', 11000, 'imagen5.jpg', 1234, 12)) //no funciona por que ya existe el codigo
-console.log(product.addProduct('Camiseta Francia', 'Descripcion del producto 6', 12599, 12345, 17))// no funciona por que faltan campos por completar
+// console.log(product.addProduct('Camiseta Brasil', 'Descripcion del producto 1', 15000, 'imagen1.jpg', 1, 32))
+// console.log(product.addProduct('Camiseta Alemania', 'Descripcion del producto 2', 12999, 'imagen2.jpg', 12, 17))
+// console.log(product.addProduct('Camiseta Italia', 'Descripcion del producto 3', 12500, 'imagen3.jpg', 123, 14))
+// console.log(product.addProduct('Camiseta Argentina', 'Descripcion del producto 4', 13000 , 'imagen1.jpg', 1234, 25))
+// console.log(product.addProduct('Camiseta Uruguay', 'Descripcion del producto 5', 11000, 'imagen5.jpg', 1234, 12)) //no funciona por que ya existe el codigo
+// console.log(product.addProduct('Camiseta Francia', 'Descripcion del producto 6', 12599, 12345, 17))// no funciona por que faltan campos por completar
 
 
 //busco todos los productos
- console.log(product.getProducts())
+//  console.log(product.getProducts())
 
 
 //busco producto por id
-console.log(product.getProductById(4))//existe
+// console.log(product.getProductById(4))//existe
 
 
 //busco producto por id
-console.log(product.getProductById(5)) //no existe
+// console.log(product.getProductById(5)) //no existe
 
 
 //actualizo producto (existe)
-console.log(product.updateProduct(2, {
-  title: 'Camiseta francia Actualizada',
-  description: 'Descripción actualizada',
-  price: 13999,
-  thumbnail: 'imagen2_actualizada.jpg',
-  code: 12,
-  stock: 10,
-}));
+// console.log(product.updateProduct(2, {
+//   title: 'Camiseta francia Actualizada',
+//   description: 'Descripción actualizada',
+//   price: 13999,
+//   thumbnail: 'imagen2_actualizada.jpg',
+//   code: 12,
+//   stock: 10,
+// }));
 
 
 //actualizo producto (no existe)
-console.log(product.updateProduct(22, {
-  title: 'Camiseta actualizada',
-  description: 'Descripción actualizada',
-  price: 13999,
-  thumbnail: 'img.jpg',
-  code: 12,
-  stock: 10,
-}));
+// console.log(product.updateProduct(22, {
+//   title: 'Camiseta actualizada',
+//   description: 'Descripción actualizada',
+//   price: 13999,
+//   thumbnail: 'img.jpg',
+//   code: 12,
+//   stock: 10,
+// }));
 
 
 //busco todos los productos
-console.log(product.getProducts())
+// console.log(product.getProducts())
 
 
-//elimino producto (existe)
-console.log(product.deleteProduct(2))
+// //elimino producto (existe)
+// console.log(product.deleteProduct(2))
 
 
 //elimino un producto (no existe)
-console.log(product.deleteProduct(6))
+// console.log(product.deleteProduct(6))
 
 
 //busco todos los elementos
-console.log(product.getProducts())
+// console.log(product.getProducts())
+
+
+export default ProductManager;
